8ad95dbcdb41c4ebfeb2044f4210674ef946a425
kelvinguchu <kelvinguchu5@gmail.com>
2023-06-27 20:43:06 +0300

    main JS functionality file

--- draw.js
+++ draw.js
@@ -1,57 +1,61 @@
-const canvas = document.getElementById("canvas"); // Get the canvas element
-const ctx = canvas.getContext("2d"); // Get the 2D rendering context
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 
-let isDrawing = false; // Flag to indicate if the user is currently drawing
-let color = "#000"; // Default color for drawing
-let brushSize = 5; // Default brush size
+let isDrawing = false;
+let color = "#000";
+let brushSize = 5;
 
-canvas.addEventListener("mousedown", startDrawing); // Start drawing when the mouse button is pressed down
-canvas.addEventListener("mouseup", stopDrawing); // Stop drawing when the mouse button is released
-canvas.addEventListener("mousemove", draw); // Draw when the mouse moves
-canvas.addEventListener("mouseout", stopDrawing); // Stop drawing when the mouse leaves the canvas
+// Add event listeners to the canvas
+canvas.addEventListener("mousedown", startDrawing); // Start drawing when mouse button is pressed down
+canvas.addEventListener("mouseup", stopDrawing); // Stop drawing when mouse button is released
+canvas.addEventListener("mousemove", draw); // Draw as the mouse moves
+canvas.addEventListener("mouseout", stopDrawing); // Stop drawing when the mouse moves out of the canvas
 
-document.getElementById("color-picker").addEventListener("input", updateColor); // Update the drawing color when the color picker value changes
-document.getElementById("brush-size").addEventListener("input", updateBrushSize); // Update the brush size when the input range value changes
-document.getElementById("eraser").addEventListener("click", useEraser); // Activate the eraser mode when the eraser button is clicked
-document.getElementById("clear").addEventListener("click", clearCanvas); // Clear the canvas when the clear button is clicked
+// Add event listeners to the controls
+document.getElementById("color-picker").addEventListener("input", updateColor); // Update the selected color
+document.getElementById("brush-size").addEventListener("input", updateBrushSize); // Update the brush size
+document.getElementById("eraser").addEventListener("click", useEraser); // Use the eraser tool
+document.getElementById("clear").addEventListener("click", clearCanvas); // Clear the canvas
 
 function startDrawing(e) {
-  isDrawing = true; // Set the drawing flag to true
-  draw(e); // Start drawing
+  isDrawing = true;
+  draw(e);
 }
 
 function stopDrawing() {
-  isDrawing = false; // Set the drawing flag to false
+  isDrawing = false;
+  ctx.beginPath(); // Clear the current path
 }
 
 function draw(e) {
-    if (!isDrawing) return; // If not drawing, exit the function
-    ctx.lineWidth = brushSize; // Set the brush size
-    ctx.strokeStyle = color; // Set the drawing color
-    ctx.lineCap = "round"; // Set the line cap style to round
-  
-    const rect = canvas.getBoundingClientRect(); // Get the canvas boundaries
-    const offsetX = e.clientX - rect.left; // Calculate the offset X
-    const offsetY = e.clientY - rect.top; // Calculate the offset Y
-  
-    ctx.lineTo(offsetX, offsetY); // Draw a line to the current mouse position
-    ctx.stroke(); // Stroke the line
-    ctx.beginPath(); // Start a new path
-    ctx.moveTo(offsetX, offsetY); // Move the drawing position to the current mouse position
+  if (!isDrawing) return;
+
+  ctx.lineWidth = brushSize;
+  ctx.strokeStyle = color;
+  ctx.lineCap = "round";
+
+  const rect = canvas.getBoundingClientRect();
+  const offsetX = e.clientX - rect.left;
+  const offsetY = e.clientY - rect.top;
+
+  ctx.lineTo(offsetX, offsetY);
+  ctx.stroke();
+  ctx.beginPath(); // Start a new path
+  ctx.moveTo(offsetX, offsetY);
 }
 
 function updateColor(e) {
-  color = e.target.value; // Update the drawing color with the selected color
+  color = e.target.value;
 }
 
 function updateBrushSize(e) {
-  brushSize = e.target.value; // Update the brush size with the selected value
+  brushSize = e.target.value;
 }
 
 function useEraser() {
-  color = "#fff"; // Set the drawing color to white, effectively acting as an eraser
+  color = "#fff";
 }
 
 function clearCanvas() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear the entire canvas
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
